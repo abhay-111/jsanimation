8876f3e20754a582c85c7a5ab5dc0160ad262b93
abhay chauhan <abhaychauhan232@gmail.com>
2020-08-25 14:22:50 +0530

    Adding the second missile animation

--- index.js
+++ index.js
@@ -22,3 +22,29 @@ function myMove() {
   
 
 }
+/* animation for missile */
+function missile() {
+    
+   
+      var missile=document.getElementById('missile')
+    var pos = window.innerWidth;
+    var pos1=window.innerWidth
+    var up=0;
+    var id = setInterval(animate1, 2);
+    function animate1() {
+      if (pos<0) {
+          pos=window.innerWidth+200
+         
+  
+      } else {
+        pos--; 
+        
+        missile.style.left = pos + 'px';
+        missile.style.rotate="225deg"
+        
+        
+      }
+  
+    }
+  
+  }
