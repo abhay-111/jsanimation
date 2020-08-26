75260133276bfc8107c2f99cede68fca94330bee
abhay chauhan <abhaychauhan232@gmail.com>
2020-08-26 14:51:08 +0530

    final commit added function to missile .Project completed.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var mposition = window.innerWidth;
 
+var m2position = window.innerWidth;
+  
 var pposition = 0;
-
+console.log(m2position,mposition)
 
     function myMove() {
         
         
       var elem = document.getElementById("plane");
-        var missile=document.getElementsByClassName('.missile')
-      
+      console.log(typeof(elem))
       var id = setInterval(animate1, 4);
       function animate1() {
 
@@ -18,12 +19,12 @@ var pposition = 0;
         } 
         else {
       
-          pposition++;
+          pposition+=2;
           setTimeout(()=>{
-            
           elem.style.rotate='360deg'
           elem.style.transition='1s'
-          },2000) 
+          },1500) 
+          
           elem.style.left = pposition + 'px';
         }
       }
@@ -34,55 +35,52 @@ var pposition = 0;
 /* animation for missile */
 
 
-
-function missile() {
-  
+setTimeout(
+  function missile() {
     
-    var missile=document.getElementById('missile')
-     
       
-    var id = setInterval(animate1, 2);
-    function animate1() {
-      if (mposition<0) {
-          mposition=window.innerWidth+200
-      } else {
-        mposition--; 
-        
-        missile.style.left = mposition + 'px';
-        missile.style.rotate="225deg"
-        
-        
-      }
-  
-    }
-  
-  }
-  
-  
-  /* animation for missile-2 */
-  function missile2() {
-    
-    console.log("misile")
-   
-      var missile=document.getElementById('missile2')
-    var m2position = window.innerWidth;
-    
-    var id = setInterval(animate1, 2);
-    function animate1() {
-      if (m2position<50) {
-          m2position=window.innerWidth+200
-         
-  
-      } else {
-        m2position--; 
-        
-        missile.style.left = m2position + 'px';
-        missile.style.rotate="225deg"
+      var missile=document.getElementById('missile')
        
         
-        
+      var id = setInterval(animate1, 2);
+      function animate1() {
+        if (mposition<0) {
+            mposition=window.innerWidth+200
+        } else {
+          mposition--; 
+          
+          missile.style.left = mposition + 'px';
+          missile.style.rotate="225deg"
+          
+          
+        }
+    
       }
-  
+    
+    },1500)
+   
+  /* animation for missile-2 */
+  setTimeout(function missile2() {
+    
+   
+    var missile=document.getElementById('missile2')
+ 
+  var id = setInterval(animate1, 2);
+  function animate1() {
+    if (m2position<50) {
+        m2position=window.innerWidth+200
+       
+
+    } else {
+      m2position--; 
+      
+      missile.style.left = m2position + 'px';
+      missile.style.rotate="225deg"
+     
+      
+      
     }
-  
+
   }
+
+},2000)
