02597c779c8abb0c966dfb91d5537d9220e3a014
abhay chauhan <abhaychauhan232@gmail.com>
2020-08-26 12:53:09 +0530

    adding a rotate function to  plane

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 var mposition = window.innerWidth;
 
 var pposition = 0;
-function myMove() {
-    
-    
-  var elem = document.getElementById("plane");
-    var missile=document.getElementsByClassName('.missile')
-  
-  var id = setInterval(animate1, 8);
-  function animate1() {
-    if (pposition>1200) {
-        pposition=0
-      
 
-    } else {
-      pposition++; 
-      elem.style.left = pposition + 'px';
+
+    function myMove() {
+        
+        
+      var elem = document.getElementById("plane");
+        var missile=document.getElementsByClassName('.missile')
       
+      var id = setInterval(animate1, 4);
+      function animate1() {
+
+        if (pposition>window.innerWidth+500) {
+          clearInterval(id)
+        location.reload()
+        } 
+        else {
+      
+          pposition++;
+          setTimeout(()=>{
+            
+          elem.style.rotate='360deg'
+          elem.style.transition='1s'
+          },2000) 
+          elem.style.left = pposition + 'px';
+        }
+      }
     }
 
-  }
-  
 
-}
- 
+
 /* animation for missile */
+
+
+
 function missile() {
+  
     
     var missile=document.getElementById('missile')
      
@@ -48,14 +59,15 @@ function missile() {
   }
   
   
+  /* animation for missile-2 */
   function missile2() {
     
     console.log("misile")
    
       var missile=document.getElementById('missile2')
     var m2position = window.innerWidth;
-    var up=0;
-    var id = setInterval(animate1, 3);
+    
+    var id = setInterval(animate1, 2);
     function animate1() {
       if (m2position<50) {
           m2position=window.innerWidth+200
